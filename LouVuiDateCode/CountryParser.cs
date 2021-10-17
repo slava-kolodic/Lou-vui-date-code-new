0957ddaf4dba356fb79df809d42f07ec51a8565e
timestockdiscounter <timestockdiscounter@gmail.com>
2021-10-17 22:17:51 +0300

    done 5

--- LouVuiDateCode/CountryParser.cs
+++ LouVuiDateCode/CountryParser.cs
@@ -11,8 +11,75 @@ namespace LouVuiDateCode
         /// <returns>An array of <see cref="Country"/> enumeration values.</returns>
         public static Country[] GetCountry(string factoryLocationCode)
         {
-            // TODO #5. Analyze unit tests for the method, and add the method implementation.
-            throw new NotImplementedException();
+            if (string.IsNullOrWhiteSpace(factoryLocationCode))
+            {
+                throw new ArgumentNullException(nameof(factoryLocationCode));
+            }
+
+            return factoryLocationCode switch
+            {
+                "A0" => new Country[] { Country.France },
+                "A1" => new Country[] { Country.France },
+                "A2" => new Country[] { Country.France },
+                "AA" => new Country[] { Country.France },
+                "AH" => new Country[] { Country.France },
+                "AN" => new Country[] { Country.France },
+                "AR" => new Country[] { Country.France },
+                "AS" => new Country[] { Country.France },
+                "BA" => new Country[] { Country.France },
+                "BJ" => new Country[] { Country.France },
+                "BU" => new Country[] { Country.France },
+                "DR" => new Country[] { Country.France },
+                "DU" => new Country[] { Country.France },
+                "DT" => new Country[] { Country.France },
+                "CO" => new Country[] { Country.France },
+                "CT" => new Country[] { Country.France },
+                "CX" => new Country[] { Country.France },
+                "ET" => new Country[] { Country.France },
+                "FL" => new Country[] { Country.France, Country.USA },
+                "LW" => new Country[] { Country.France, Country.Spain },
+                "MB" => new Country[] { Country.France },
+                "MI" => new Country[] { Country.France },
+                "NO" => new Country[] { Country.France },
+                "RA" => new Country[] { Country.France },
+                "RI" => new Country[] { Country.France },
+                "SD" => new Country[] { Country.France, Country.USA },
+                "SF" => new Country[] { Country.France },
+                "SL" => new Country[] { Country.France },
+                "SN" => new Country[] { Country.France },
+                "SP" => new Country[] { Country.France },
+                "SR" => new Country[] { Country.France },
+                "TJ" => new Country[] { Country.France },
+                "TH" => new Country[] { Country.France },
+                "TR" => new Country[] { Country.France },
+                "TS" => new Country[] { Country.France },
+                "VI" => new Country[] { Country.France },
+                "VX" => new Country[] { Country.France },
+                "LP" => new Country[] { Country.Germany },
+                "OL" => new Country[] { Country.Germany },
+                "BC" => new Country[] { Country.Italy },
+                "BO" => new Country[] { Country.Italy },
+                "CE" => new Country[] { Country.Italy },
+                "FO" => new Country[] { Country.Italy },
+                "MA" => new Country[] { Country.Italy },
+                "OB" => new Country[] { Country.Italy },
+                "RC" => new Country[] { Country.Italy },
+                "RE" => new Country[] { Country.Italy },
+                "SA" => new Country[] { Country.Italy },
+                "TD" => new Country[] { Country.Italy },
+                "CA" => new Country[] { Country.Spain },
+                "LO" => new Country[] { Country.Spain },
+                "LB" => new Country[] { Country.Spain },
+                "LM" => new Country[] { Country.Spain },
+                "GI" => new Country[] { Country.Spain },
+                "DI" => new Country[] { Country.Switzerland },
+                "FA" => new Country[] { Country.Switzerland },
+                "FC" => new Country[] { Country.USA },
+                "FH" => new Country[] { Country.USA },
+                "LA" => new Country[] { Country.USA },
+                "OS" => new Country[] { Country.USA },
+                _ => throw new ArgumentOutOfRangeException(nameof(factoryLocationCode)),
+            };
         }
     }
 }
